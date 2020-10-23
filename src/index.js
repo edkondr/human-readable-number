3c2879e000aab645752fb95b7205c25393384b60
edkondr <edyard2002@mail.ru>
2020-10-23 22:09:22 +0300

    fix: change files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
- 
+ module.exports = function toReadable (number) {
+    const digit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const digits = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    function fanc(number) {
+      if (number <= 19) {
+        return digit[number]}
+      else if (number <= 99) {return (digits[Math.floor(number / 10)] + ' ' + digit[number % 10]) .split(' ').filter(f => f).join(' ')}
+      };
 
-
-module.exports = function toReadable (number) {
-    var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-    var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
- 
-    if ((number = number.toString()).length > 9) return 'overflow';
-    n = ('000000000' + number).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-    if (!n) return; var str = '';
-    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
-    return str;
+    if(number == 0){
+      return 'zero'}
+    else if(number <= 99) { 
+      return fanc(number)}
+    else if(number <= 999) {
+      return digit[Math.floor(number % 1000 / 100)] + ' ' + 'hundred' + ((number % 100) ? ' ' + fanc(number % 100) : '')};
+    
 }
-document.getElementById('number').onkeyup = function () {
-    document.getElementById('words').innerHTML = inWords(document.getElementById('number').value);
-};
